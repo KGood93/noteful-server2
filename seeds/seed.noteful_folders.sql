9b02acbaf9f0e02cbcc987ead684a644f4505448
KGood93 <goodreau.kath@gmail.com>
2019-12-29 19:18:57 -0600

    Add seed files and connect database

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,7 @@
+INSERT INTO noteful_folders (name)
+    VALUES
+        ('Test Folder One'),
+        ('Test Folder Two'),
+        ('Test Folder Three'),
+        ('Test Folder Four'),
+        ('Test Folder Five')
---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,11 @@
+INSERT INTO noteful_notes (name, content, folder_id)
+    VALUES 
+        ('Note One', 'Note One, Folder One', 1),
+        ('Note Two', 'Note Two, Folder One', 1),
+        ('Note Three', 'Note Three, Folder Two', 2),
+        ('Note Four', 'Note Four, Folder Three', 3),
+        ('Note Five', 'Note Five, Folder Three', 3),
+        ('Note Six', 'Note Six, Folder Four', 4),
+        ('Note Seven', 'Note Seven, Folder Five', 5),
+        ('Note Eight', 'Note Eight, Folder Five', 5),
+        ('Note Nine', 'Note Nine, Folder Five', 5)
